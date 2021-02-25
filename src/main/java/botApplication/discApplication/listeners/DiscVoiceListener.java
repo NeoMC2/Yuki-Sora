45821aefb8a769b10d906ef14398848fb9b1272a
Noah Till <NTill@gmx.de>
2021-02-25 11:48:19 +0100

    Update DiscVoiceListener.java

--- src/main/java/botApplication/discApplication/listeners/DiscVoiceListener.java
+++ src/main/java/botApplication/discApplication/listeners/DiscVoiceListener.java
@@ -173,31 +173,35 @@ public class DiscVoiceListener extends ListenerAdapter {
             } catch (Exception e){
             }
             if(vc != null) {
-                if(!initServers.contains(vc.getGuild().getId())){
-                    initServers.add(vc.getGuild().getId());
-                    DiscApplicationServer server = DiscUtilityBase.lookForServer(vc.getGuild(), engine);
-                    if(server != null){
-                        for (String id:server.getAutoChannels()) {
-                            VoiceChannel auto = engine.getDiscEngine().getBotJDA().getVoiceChannelById(id);
-                            if(auto != null){
-                                if(auto.getMembers().size() >0){
-                                    Member m0 = null;
-                                    m0 = auto.getMembers().get(0);
-                                    auto.getGuild().moveVoiceMember(m0, auto).complete();
-                                    if(auto.getMembers().size() > 1)
+                active.add(vc);
+            }
+        }
+
+        for (Guild g:engine.getDiscEngine().getBotJDA().getGuilds()) {
+            if(!initServers.contains(g.getId())){
+                initServers.add(g.getId());
+                DiscApplicationServer server = DiscUtilityBase.lookForServer(g, engine);
+                if(server != null){
+                    for (String id:server.getAutoChannels()) {
+                        VoiceChannel auto = engine.getDiscEngine().getBotJDA().getVoiceChannelById(id);
+                        if(auto != null){
+                            if(auto.getMembers().size() >0){
+                                Member m0 = null;
+                                m0 = auto.getMembers().get(0);
+                                auto.getGuild().moveVoiceMember(m0, auto).complete();
+                                if(auto.getMembers().size() > 1)
                                     for (int i = 1; i < auto.getMembers().size(); i++) {
                                         try {
                                             auto.getGuild().moveVoiceMember(auto.getMembers().get(i), m0.getVoiceState().getChannel()).queue();
                                         } catch (Exception ignored){
                                         }
                                     }
-                                }
                             }
                         }
                     }
                 }
-                active.add(vc);
             }
         }
+
     }
 }
